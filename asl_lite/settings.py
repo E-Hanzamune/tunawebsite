e4744bccc52f5c4254786e9b0bf21381bf752759
krauqx <namucodwightjustin@outlook.com>
2025-11-05 23:21:41 +0800

    Fix STATICFILES_DIRS path and disable collectstatic

--- asl_lite/settings.py
+++ asl_lite/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "translator/static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / 'translator' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
