62cdd136675c526663bce142ca8ef2092df5f718
krauqx <namucodwightjustin@outlook.com>
2025-11-05 23:07:38 +0800

    Add static file settings and collectstatic output

--- asl_lite/settings.py
+++ asl_lite/settings.py
@@ -127,13 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / "translator/static"]
+STATICFILES_DIRS = [BASE_DIR / 'translator' / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
