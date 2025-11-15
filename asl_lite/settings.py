db485b571e3247f6cb7e737d84adc698bc037838
Erwin PC <101384095+MeguMegu02@users.noreply.github.com>
2025-11-15 09:37:28 +0800

    Initial Commit

--- asl_lite/settings.py
+++ asl_lite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^bx2dsfuuc%8*$5pq88&i87zor0d41tbt64#5nwea8_u$_*3+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'translator' / 'static']
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
